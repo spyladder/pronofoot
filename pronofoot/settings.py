b455e08ed6fbde79db9c5828c5c70988b9df8851
spyladder <spyladder@gmail.com>
2016-05-24 11:47:03 +0200

    Disabled dynamic time zone and fixed local time zone.

--- pronofoot/settings.py
+++ pronofoot/settings.py
@@ -115,13 +115,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
